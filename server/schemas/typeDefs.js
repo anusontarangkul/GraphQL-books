8ee53cb2851f8441bdd387ff0f459c2252159972
David Anusontarangkul <anusontarangkul.d@gmail.com>
2021-08-06 00:09:52 -0700

    add typeDefs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,20 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+    type Book {
+        _id: ID
+        title: String
+        authors: String
+        pages: Int
+    }
+    type Query {
+        books: [Book]
+        book(title: String!): Book
+    }
+
+    type Mutation {
+        addBook(title: String!, author: String!, pages: Int!): Book
+    }
+`
+
+module.exports = typeDefs;
