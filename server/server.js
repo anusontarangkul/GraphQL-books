0b0f6e981d6933fc740344f26d96c01223a87c18
David Anusontarangkul <anusontarangkul.d@gmail.com>
2021-08-05 22:32:37 -0700

    set up server

---
+++ server/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+const { ApollServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas');
+
+const db = require('./config/connection');
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+const server = new ApollServer({
+    typeDefs,
+    resolvers
+});
+
+server.applyMiddleware({ app });
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+db.once('open', () => {
+    console.log('DB is live');
+    app.listen(PORT, () => {
+        console.log(`App is running on ${PORT}`);
+        console.log(`The graphQL PLAYGROUND that is configured here http://loccalhost:${PORT}${server.graphqlPath}`)
+    })
+})
